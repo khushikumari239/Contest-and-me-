1d28b09ca8255387619d03578b7357f2d439eb51
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-04-26 16:06:24 +0530

    Add new files

---
+++ prdct2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a,b;
+    // int n;
+    cin>>a;
+    cin>>b;
+    int n = a*b;
+    cout<<n<<endl;
+
+    if(n%2==0) 
+        cout <<"EVEN"<< endl;
+        else 
+        cout<<"ODD"<< endl;
+    
+
+
+    return 0;
+}
